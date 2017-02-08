ebc5564c0843afe2db5e5099624fd330b20f63b8
gracielatham <gracie_latham1997@yahoo.com>
2017-02-08 13:57:58 -0600

    Create main.cpp

---
+++ program_2/main.cpp
@@ -0,0 +1,46 @@
+/**
+* Program-2: Image Manipilation 2
+* Gracie Latham
+* Description: 
+*     This program reads in images stored as rgb values in a space delimited file format.
+* Course: 1063 Data Structures
+* Semester: Spring 2017
+* Date: 15 02 2017
+*/
+
+class ImageManip{
+private:
+    rgb** image;
+    int width;
+    int height;
+    ifstream infile;
+    ofstream ofile;
+
+public:
+    ImageManip(){
+    }
+
+    void readFile(){
+    }
+
+    void writeFile(){
+    }
+
+    void grayScale(){
+         // your code
+    }
+
+    void flipVert(){
+        // your code
+    }
+
+    void flipHorz(){
+        // your code
+    }
+
+    void grayScale(){
+        // your code
+    }
+    ~ImagerManip(){
+    }
+}
