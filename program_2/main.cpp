ead8a7bf69d915145e0ec51efc09f5e681d05a1f
gracielatham <gracie_latham1997@yahoo.com>
2017-02-11 13:44:16 -0600

    Update main.cpp

--- program_2/main.cpp
+++ program_2/main.cpp
@@ -24,14 +24,34 @@ private:
     rgb** image;
     int width;
     int height;
-    ifstream infile;
+    ifstream ifile;
     ofstream ofile;
     string ifile_name;
     string ofile_name;
 
+    /**
+* @FunctionName: readFile
+* @Description: 
+*     Reads in the input file
+* @Params:
+*    none
+* @Returns:
+*    void
+*/
+    
     void readFile(){
     }
 
+/**
+* @FunctionName: writeFile
+* @Description: 
+*     writes new data into file
+* @Params:
+*    none
+* @Returns:
+*    void
+*/
+    
     void writeFile(){
     }
 
@@ -39,25 +59,61 @@ public:
     ImageManip(){
     }
 
+/**
+* @FunctionName: flipVert
+* @Description: 
+*     Flips the image vertically
+* @Params:
+*   string ifile_name - takes data from infile
+*   string ofile_name - prints data to outfile
+* @Returns:
+*    void
+*/
+    
     void flipVert(string ifile_name,string ofile_name){
 
     }
 
+/**
+* @FunctionName: flipHorz
+* @Description: 
+*    Flips the image horizontally 
+* @Params:
+*  string ifile_name - takes data from infile
+*  string ofile_name - prints data to outfile
+* @Returns:
+*    void
+*/
+    
     void flipHorz(string ifile_name,string ofile_name){
        
     }
 
+/**
+* @FunctionName: grayScale
+* @Description: 
+*     Loops through a 2D array and turns every RGB value into its grayscale equivalent.
+* @Params:
+*  string ifile_name - takes data from infile
+*  string ofile_name - prints data to outfile
+* @Returns:
+*    void
+*/
+    
     void grayScale(string ifile_name,string ofile_name){
        
     }
     ~ImagerManip(){
     }
-}
+};
 
 int main() {
+    ifstream ifile;
+    ofstream ofile;
     ifile.open("bot.txt");
     ofile.open("bot2.txt");  
     
 
   return 0;
     
+}
