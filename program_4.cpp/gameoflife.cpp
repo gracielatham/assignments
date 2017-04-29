de6ea5b3ea56161279e681d74b67171ff3ff3550
gracielatham <gracie_latham1997@yahoo.com>
2017-04-29 11:05:59 -0500

    Update gameoflife.cpp

--- program_4.cpp/gameoflife.cpp
+++ program_4.cpp/gameoflife.cpp
@@ -1,3 +1,14 @@
+/**
+* Program-4: Game of Life
+* Gracie Latham 
+* Description:
+*     This program modifies the game of life to create a glider gun and cause the moving spaces to
+pass through the bottem back to the top
+* Course: 1063 Data Structures
+* Semester: Spring 2017
+* Date:05 01 2017
+*/
+
 #include <iostream>
 #include <string>
 #include <fstream>
