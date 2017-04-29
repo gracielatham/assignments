5b7c4ad4fb43283d2b851f6178c56b68a7442f5a
gracielatham <gracie_latham1997@yahoo.com>
2017-04-29 11:07:38 -0500

    Update gameoflife.cpp

--- program_4.cpp/gameoflife.cpp
+++ program_4.cpp/gameoflife.cpp
@@ -8,7 +8,6 @@ pass through the bottem back to the top
 * Semester: Spring 2017
 * Date:05 01 2017
 */
-
 #include <iostream>
 #include <string>
 #include <fstream>
@@ -87,15 +86,34 @@ public:
     
     int CountNeighbors(int row, int col){
         int neighbors = 0;
+        int up, down, left, right;
+        
+        if(row == 0){
+          up = Rows - 1;
+        }else{
+          up = (row + 1) % Rows; 
+        }
+          
+          down = (row + 1) % Rows;
+          
+        if(col == 0){
+          left = Cols - 1; 
+        }else{
+          left = (col + 1) % Cols; 
+        }
+        
+        right = (col + 1) % Cols; 
+        
         if (Board[row][col] == 1){
             neighbors--;
         }
-        for (int i = row - 1; i <= row + 1; i++){
-            for (int j = col - 1; j <= col + 1; j++){
-                if (OnBoard(i, j)){
-                    neighbors += Board[i][j];
-                }
+  
+        for(int i = up; i != (down + 1) % Rows; i = (i + 1) % Rows){
+          for(int j = left; j != (right + 1) % Cols; j = (j + 1) % Cols){
+            if(OnBoard(i, j)){
+              neighbors += Board[i][j];
             }
+          }
         }
         return neighbors;
     }
@@ -136,18 +154,23 @@ public:
     void Run(int generations=99999){
         int neighbors = 0;
         int g = 0;
-        while (g < generations){
+        bool Stable = false;
+        while (g < generations && Stable == false){
             for (int i = 0; i < Rows; i++){
                 for (int j = 0; j < Cols; j++){
                     neighbors = CountNeighbors(i, j);
                     if (Board[i][j] == 1 && (neighbors < 2 || neighbors > 3)){
                         Board2[i][j] = -1;
                     }
+                    if (Board[i][j] == 1 && iscorner(i, j)){
+                      Board[i][j] = -1;
+                    }
                     if (Board[i][j] == 0 && neighbors == 3){
                         Board2[i][j] = 1;
                     }
                 }
             }
+            Stable = stable(); 
             AddGens();
             usleep(100000);
             clear_screen(30);
@@ -156,23 +179,56 @@ public:
         }
         
     }
+    
+    bool iscorner(int row, int col){
+      if ((row == 0) || row == Rows - 1 && (col == 0) || col == Cols - 1)
+          return true;
+      
+      return false; 
+    }
+    
+    bool stable(){
+      for (int i = 0; i < Rows; i++){
+        for (int j = 0; j < Cols; j++){
+           
+           if(Board[i][j] != Board[i][j] + Board2[i][j])
+              return false;
+         }
+      }
+      
+      return true; 
+    
+    }
+    
+    void glidergun(){
+      if(Rows >= 11 && Cols >= 38){
+        ResetBoardArray(Board);
+        ResetBoardArray(Board2);
+        string line;
+        char ch;
+        ifstream fin;
+        fin.open("glider.txt");
+        for (int i = 0; i <= 11; i++){
+            for (int j = 0; j <= 38; j++){
+                fin.get(ch);
+                if (ch == 10){
+                    continue;
+                }
+                Board[i][j] = int(ch) - 48;
+            }
+        }
+        fin.close();
+      }
+      
+    }
 };
 
 int main(){
-    //GameOfLife G(26, 40);
+  
+    GameOfLife.open("gen_one.txt");
+    
+    G.glidergun();
 
-    GameOfLife G("data2.txt");
-
-    // G.RandomPopulate(60);
-    // G.SetCell(0, 5, 1);
-    // G.SetCell(0, 6, 1);
-    // G.SetCell(0, 7, 1);
-    // G.SetCell(1, 5, 1);
-    // G.SetCell(1, 6, 1);
-    // G.SetCell(1, 7, 1);
-    // G.SetCell(2, 5, 1);
-    // G.SetCell(2, 6, 1);
-    // G.SetCell(2, 7, 1);
-    G.Run(100);
+    G.Run(100); 
     return 0;
 }
